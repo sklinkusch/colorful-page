513380967caaa18a63e8c7eec34dc3350b74dea5
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-05-04 15:24:21 +0200

    add trycatch around color changing function

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import './App.css';
 function App() {
   const [color, setColor] = useState("#ffffff")
   const changeColor = () => {
-    const widthRaw = window.innerWidth || 0
-    const heightRaw = window.innerHeight || 0
-    const width = widthRaw > 4095 ? 4095 : widthRaw
-    const height = heightRaw > 4095 ? 4095 : heightRaw
-    const widthStringRaw = width.toString(16)
-    const heightStringRaw = height.toString(16)
-    const heightString = heightStringRaw.length === 1 
-      ? `00${heightStringRaw}` 
-      : heightStringRaw.length === 2
-        ? `0${heightStringRaw}`
-        : `${heightStringRaw}`
-    const widthString = widthStringRaw.length === 1 
-      ? `00${widthStringRaw}` 
-      : widthStringRaw.length === 2
-        ? `0${widthStringRaw}`
-        : `${widthStringRaw}`
-    const myColor = "#" + widthString + heightString
-    setColor(myColor)
+    try {
+      const widthRaw = window.innerWidth || 0
+      const heightRaw = window.innerHeight || 0
+      const width = widthRaw > 4095 ? 4095 : widthRaw
+      const height = heightRaw > 4095 ? 4095 : heightRaw
+      const widthStringRaw = width.toString(16)
+      const heightStringRaw = height.toString(16)
+      const heightString = heightStringRaw.length === 1 
+        ? `00${heightStringRaw}` 
+        : heightStringRaw.length === 2
+          ? `0${heightStringRaw}`
+          : `${heightStringRaw}`
+      const widthString = widthStringRaw.length === 1 
+        ? `00${widthStringRaw}` 
+        : widthStringRaw.length === 2
+          ? `0${widthStringRaw}`
+          : `${widthStringRaw}`
+      const myColor = "#" + widthString + heightString
+      setColor(myColor)
+    } catch(error) {
+      setColor("#ffffff")
+    }
   }
   window.addEventListener("load", changeColor)
   window.addEventListener("resize", changeColor)
